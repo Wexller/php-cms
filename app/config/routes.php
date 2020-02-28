6f007f7a7221ba586e8e63e063a7397571a40603
Maxim Davydov <m.davydov@make.st>
2020-02-28 22:29:42 +0700

    Add rotes list in config and add logic to Router class

---
+++ app/config/routes.php
@@ -0,0 +1,23 @@
+<?
+
+return array(
+  '' => array(
+    'controller' => 'main',
+    'action' => 'index',
+  ),
+
+  'account/login' => array(
+    'controller' => 'account',
+    'action' => 'login',
+  ),
+
+  'account/register' => array(
+    'controller' => 'account',
+    'action' => 'register',
+  ),
+
+  'news/show' => array(
+    'controller' => 'news',
+    'action' => 'show',
+  ),
+);
--- app/core/Router.php
+++ app/core/Router.php
@@ -9,18 +9,39 @@ class Router {
 
   function __construct()
   {
-    //
+    $arr = require_once 'app/config/routes.php';
+    foreach ($arr as $key => $val) {
+      $this->add($key, $val);
+    }
   }
 
-  function add() {
-    //
+  public function add($route, $params) {
+    $route = "#^$route$#";
+    $this->routes[$route] = $params;
   }
 
   function match() {
-    //
+    $url = trim($_SERVER['REQUEST_URI'], '/');
+    foreach ($this->routes as $route => $params) {
+      if (preg_match($route, $url, $matches)) {
+        $this->params = $params;
+        return true;
+      }
+    }
+
+    return false;
   }
 
   function run() {
-    echo 'start';
+    if ($this->match()) {
+      $controller = 'app\controllers\\' . ucfirst($this->params['controller'] . 'Controller.php');
+      if (class_exists($controller)) {
+        echo 'OK';
+      } else {
+        echo 'Class not found' . $controller;
+      }
+    } else {
+      echo 'Rote not found';
+    }
   }
 }
