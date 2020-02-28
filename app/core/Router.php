d219cd309f7b1862dfdc5938c6a85354a1d61cca
Maxim Davydov <m.davydov@make.st>
2020-02-28 21:52:57 +0700

    Add dev settings and Router class

---
+++ app/core/Router.php
@@ -0,0 +1,26 @@
+<?
+
+namespace app\core;
+
+class Router {
+
+  protected $routes = [];
+  protected $params = [];
+
+  function __construct()
+  {
+    //
+  }
+
+  function add() {
+    //
+  }
+
+  function match() {
+    //
+  }
+
+  function run() {
+    echo 'start';
+  }
+}
--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?
-echo 'Empty project';
+require_once 'app/lib/Dev.php';
+
+use app\core\Router;
+
+spl_autoload_register(function ($class) {
+  $path = str_replace('\\', '/', $class) . '.php';
+  if (file_exists($path)) {
+    require $path;
+  }
+});
+
+session_start();
+
+$router = new Router();
+$router->run();
