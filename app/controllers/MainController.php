dc62dad2acfae329f649cd16f04505b96384269a
Maxim Davydov <m.davydov@make.st>
2020-03-04 22:02:56 +0700

    Add class DataBase

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,9 +3,20 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\lib\DataBase;
 
 Class MainController extends Controller {
     public function indexAction() {
+      $db = new DataBase();
+
+      $params = array(
+        'id' => 2,
+        'name' => 'Петя',
+      );
+
+      $data = $db->column('SELECT name FROM users WHERE id = :id AND name = :name', $params);
+      var_dump($data);
+
       $this->view->render('Main page');
     }
 }
